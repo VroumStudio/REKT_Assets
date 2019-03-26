edb36037c8dfdfbab57b932f52c50aec7c480804
Timothé Beaugelin <39568935+tbeaugelin@users.noreply.github.com>
2019-03-26 16:14:30 +0100

    t

--- Garage/ExportedFbx/ImportAssetScript.py
+++ Garage/ExportedFbx/ImportAssetScript.py
@@ -33,6 +33,75 @@ print('========================= Import started ! =========================')
 
 
 
+################[ Import Rolling_Drawer as StaticMesh type ]################
+fbx_factory = PyFbxFactory()
+fbx_factory.ImportUI.bImportMaterials = True
+fbx_factory.ImportUI.bImportTextures = False
+fbx_factory.ImportUI.TextureImportData.MaterialSearchLocation = EMaterialSearchLocation.Local
+fbx_factory.ImportUI.StaticMeshImportData.bCombineMeshes = True
+fbx_factory.ImportUI.StaticMeshImportData.bAutoGenerateCollision = True
+FbxLoc = os.path.join(r'D:\Users\etudiant\Documents\GitHub\REKT_Assets\Garage\ExportedFbx\StaticMesh\SM_Rolling_Drawer.fbx')
+AdditionalParameterLoc = os.path.join(r'D:\Users\etudiant\Documents\GitHub\REKT_Assets\Garage\ExportedFbx\StaticMesh\SM_Rolling_Drawer_AdditionalParameter.ini')
+AssetImportLocation = (os.path.join(UnrealImportLocation, r'').replace('\\','/')).rstrip('/')
+try:
+	asset = fbx_factory.factory_import_object(FbxLoc, AssetImportLocation)
+	lods_to_add = GetOptionByIniFile(AdditionalParameterLoc, 'LevelOfDetail')
+	for x, lod in enumerate(lods_to_add):
+		asset.static_mesh_import_lod(lod, x+1)
+	asset.save_package()
+	asset.post_edit_change()
+	ImportedAsset.append(asset)
+except:
+	ImportedAsset.append('Import error for asset named "Rolling_Drawer" ')
+
+
+
+################[ Import Desk_Complex as StaticMesh type ]################
+fbx_factory = PyFbxFactory()
+fbx_factory.ImportUI.bImportMaterials = True
+fbx_factory.ImportUI.bImportTextures = False
+fbx_factory.ImportUI.TextureImportData.MaterialSearchLocation = EMaterialSearchLocation.Local
+fbx_factory.ImportUI.StaticMeshImportData.bCombineMeshes = True
+fbx_factory.ImportUI.StaticMeshImportData.bAutoGenerateCollision = True
+FbxLoc = os.path.join(r'D:\Users\etudiant\Documents\GitHub\REKT_Assets\Garage\ExportedFbx\StaticMesh\SM_Desk_Complex.fbx')
+AdditionalParameterLoc = os.path.join(r'D:\Users\etudiant\Documents\GitHub\REKT_Assets\Garage\ExportedFbx\StaticMesh\SM_Desk_Complex_AdditionalParameter.ini')
+AssetImportLocation = (os.path.join(UnrealImportLocation, r'').replace('\\','/')).rstrip('/')
+try:
+	asset = fbx_factory.factory_import_object(FbxLoc, AssetImportLocation)
+	lods_to_add = GetOptionByIniFile(AdditionalParameterLoc, 'LevelOfDetail')
+	for x, lod in enumerate(lods_to_add):
+		asset.static_mesh_import_lod(lod, x+1)
+	asset.save_package()
+	asset.post_edit_change()
+	ImportedAsset.append(asset)
+except:
+	ImportedAsset.append('Import error for asset named "Desk_Complex" ')
+
+
+
+################[ Import Workstation as StaticMesh type ]################
+fbx_factory = PyFbxFactory()
+fbx_factory.ImportUI.bImportMaterials = True
+fbx_factory.ImportUI.bImportTextures = False
+fbx_factory.ImportUI.TextureImportData.MaterialSearchLocation = EMaterialSearchLocation.Local
+fbx_factory.ImportUI.StaticMeshImportData.bCombineMeshes = True
+fbx_factory.ImportUI.StaticMeshImportData.bAutoGenerateCollision = True
+FbxLoc = os.path.join(r'D:\Users\etudiant\Documents\GitHub\REKT_Assets\Garage\ExportedFbx\StaticMesh\SM_Workstation.fbx')
+AdditionalParameterLoc = os.path.join(r'D:\Users\etudiant\Documents\GitHub\REKT_Assets\Garage\ExportedFbx\StaticMesh\SM_Workstation_AdditionalParameter.ini')
+AssetImportLocation = (os.path.join(UnrealImportLocation, r'').replace('\\','/')).rstrip('/')
+try:
+	asset = fbx_factory.factory_import_object(FbxLoc, AssetImportLocation)
+	lods_to_add = GetOptionByIniFile(AdditionalParameterLoc, 'LevelOfDetail')
+	for x, lod in enumerate(lods_to_add):
+		asset.static_mesh_import_lod(lod, x+1)
+	asset.save_package()
+	asset.post_edit_change()
+	ImportedAsset.append(asset)
+except:
+	ImportedAsset.append('Import error for asset named "Workstation" ')
+
+
+
 ################[ Import Armature as StaticMesh type ]################
 fbx_factory = PyFbxFactory()
 fbx_factory.ImportUI.bImportMaterials = True
